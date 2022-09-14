a401c4623e967ff6b448be40cad6b9e8ce09c809
alea2807 <70931822+alea2807@users.noreply.github.com>
2022-09-14 13:16:04 +0300

    Add files via upload

---
+++ probl 3.py
@@ -0,0 +1,7 @@
+a,b,c,d=int(input()),int(input()),int(input()),int(input())
+def fractie(a,b,c,d):
+    inm = ((a*c)/ (b*d))
+    sum = (((a*c)+(c*b))/ (b*c))
+    print(inm,sum)
+fractie(a,b,c,d)
+
